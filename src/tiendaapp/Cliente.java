a217524e1472ec7b335c7ee4bc414a3221fd3da0
Jonathan Salazar <salazarbarrrerajonathan99@gmail.com>
2026-06-25 20:30:17 -0600

    Implemento lógica completa de Producto, Cliente y TiendaApp

--- src/tiendaapp/Cliente.java
+++ src/tiendaapp/Cliente.java
@@ -1,13 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tiendaapp;
 
-/**
- *
- * @author covic
- */
+import java.util.ArrayList;
+
+
 public class Cliente {
-    
+private String nombre;
+private int id;
+private ArrayList<String> historialCompras;
+
+    public Cliente(String nombre, int id) {
+        this.nombre = nombre;
+        this.id = id;
+        this.historialCompras = new ArrayList<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public ArrayList<String> getHistorialCompras() {
+        return historialCompras;
+    }
+
+ public void agregarCompra(String descripcion){
+     historialCompras.add(descripcion);
+ }
+ 
+ public void mostrarHistorial(){
+     boolean vacia = historialCompras.isEmpty();
+     if(vacia){
+         System.out.println("El historial esta vacio");
+     }
+     else{
+         for(String historial:historialCompras){
+             System.out.println("-"+historial);
+         }
+     }
+ }
+ 
+
 }
--- src/tiendaapp/Producto.java
+++ src/tiendaapp/Producto.java
@@ -1,6 +1,48 @@
 package tiendaapp;
 
 public class Producto {
+    private String nombre;
+    private double precio;
+    private int stock;
+
+    public Producto(String nombre, double precio, int stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
     
+    public boolean reducirStock(int cantidad){
+        boolean hayStock=true;
+        if(stock>=cantidad){
+        this.stock-=cantidad;
+        hayStock=true;
+        }
+        else{
+            System.out.println("No hay stock");
+            hayStock=false;
+        }
+        return hayStock;
+    }
     
+    public void mostrarInfo(){
+          System.out.println("El producto "+ nombre
+          +" su precio es "+precio+" y el stock disponible es "
+          +stock);  
+        }
 }
